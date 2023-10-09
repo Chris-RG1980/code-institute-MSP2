16cdc7fe607504010a7cbc3ac12eda2cb20ba074
Chris Grabham <chrisgrabham@outlook.com>
2023-10-09 15:37:54 +0100

    feat: Initialise game data

--- assets/scripts/memorygame.js
+++ assets/scripts/memorygame.js
@@ -1,7 +1,7 @@
 class MemoryGame {
 
-  constructor(test) {
-    this.test = test;
+  constructor(gameData) {
+    this.gameData = gameData;
   }
 
   start() {
@@ -20,14 +20,3 @@ class MemoryGame {
 
   }
 };
-
-const dataArray = [
-  {id: 1, image: 'black-panther.webp' },
-  {id: 2, image: 'captain-america.webp' },
-  {id: 3, image: 'captain-marvel.webp' },
-  {id: 4, image: 'groot.webp' },
-  {id: 5, image: 'incredible-hulk.webp'},
-  {id: 6, image: 'iron-man.webp' },
-  {id: 7, image: 'spiderman.webp' },
-  {id: 8, image: 'thor.webp'}
-];
