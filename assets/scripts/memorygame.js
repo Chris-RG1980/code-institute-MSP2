fb58d08dfa6ab786b8676c2db05d4ceda63f7b31
Chris Grabham <chrisgrabham@outlook.com>
2023-10-09 15:09:30 +0100

    feat: Create game data

--- assets/scripts/memorygame.js
+++ assets/scripts/memorygame.js
@@ -22,12 +22,12 @@ class MemoryGame {
 };
 
 const dataArray = [
-  {id: , image: },
-  {id: , image: },
-  {id: , image: },
-  {id: , image: },
-  {id: , image: },
-  {id: , image: },
-  {id: , image: },
-  {id: , image: }
+  {id: 1, image: 'black-panther.webp' },
+  {id: 2, image: 'captain-america.webp' },
+  {id: 3, image: 'captain-marvel.webp' },
+  {id: 4, image: 'groot.webp' },
+  {id: 5, image: 'incredible-hulk.webp'},
+  {id: 6, image: 'iron-man.webp' },
+  {id: 7, image: 'spiderman.webp' },
+  {id: 8, image: 'thor.webp'}
 ];
